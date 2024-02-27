9ae7fdc0350eaed21d8a0ab560c950204ccc7297
Naokitsu <naokitsu@tutanota.com>
2024-02-27 23:15:21 +0400

    Parsing tree

--- src/lib.rs
+++ src/lib.rs
@@ -10,20 +10,97 @@ mod tests;
 mod token;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-enum Operations {
-    Plus,
-    Minus,
+enum Operation {
+    UnaryPlus,
+    BinaryPlus,
+    UnaryMinus,
+    BinaryMinus,
     Multiply,
     Divide,
 }
 
-impl PartialOrd for Operations {
+impl PartialOrd for Operation {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         match (self, other) {
-            (Operations::Plus | Operations::Minus, Operations::Plus | Operations::Minus) => Some(Ordering::Equal),
-            (Operations::Multiply | Operations::Divide, Operations::Multiply | Operations::Divide) => Some(Ordering::Equal),
-            (Operations::Plus | Operations::Minus, Operations::Multiply | Operations::Divide) => Some(Ordering::Greater),
-            (Operations::Multiply | Operations::Divide, Operations::Plus | Operations::Minus) => Some(Ordering::Less),
+            (Operation::UnaryPlus | Operation::BinaryPlus | Operation::UnaryMinus | Operation::BinaryMinus, Operation::UnaryPlus | Operation::BinaryPlus | Operation::UnaryMinus | Operation::BinaryMinus) => Some(Ordering::Equal),
+            (Operation::Multiply | Operation::Divide, Operation::Multiply | Operation::Divide) => Some(Ordering::Equal),
+            (Operation::UnaryPlus | Operation::BinaryPlus | Operation::UnaryMinus | Operation::BinaryMinus, Operation::Multiply | Operation::Divide) => Some(Ordering::Greater),
+            (Operation::Multiply | Operation::Divide, Operation::UnaryPlus | Operation::BinaryPlus | Operation::UnaryMinus | Operation::BinaryMinus) => Some(Ordering::Less),
+        }
+    }
+}
+
+fn collapse(operation: Operation, nodes: &mut Vec<Node>) {
+    match operation {
+        Operation::UnaryPlus => {
+            let prev = nodes.pop().unwrap();
+            nodes.push(
+                Node::Unary {
+                    child: Box::new(prev),
+                    strategy: |child, state| Ok(child.calculate(state)?),
+                }
+            );
+        }
+        Operation::UnaryMinus => {
+            let prev = nodes.pop().unwrap();
+            nodes.push(
+                Node::Unary {
+                    child: Box::new(prev),
+                    strategy: |child, state| Ok(-child.calculate(state)?),
+                }
+            );
+        }
+        Operation::BinaryPlus => {
+            let prev_top = nodes.pop().unwrap();
+            let prev_bot = nodes.pop().unwrap();
+            nodes.push(Node::Binary {
+                left: Box::new(prev_bot),
+                right: Box::new(prev_top),
+                strategy: |left, right, state| {
+                    let l = left.calculate(state)?;
+                    let r = right.calculate(state)?;
+                    Ok(l + r)
+                },
+            });
+        }
+        Operation::BinaryMinus => {
+            let prev_top = nodes.pop().unwrap();
+            let prev_bot = nodes.pop().unwrap();
+            nodes.push(Node::Binary {
+                left: Box::new(prev_bot),
+                right: Box::new(prev_top),
+                strategy: |left, right, state| {
+                    let l = left.calculate(state)?;
+                    let r = right.calculate(state)?;
+                    Ok(l - r)
+                },
+            });
+        }
+        Operation::Multiply => {
+            let prev_top = nodes.pop().unwrap();
+            let prev_bot = nodes.pop().unwrap();
+            nodes.push(Node::Binary {
+                left: Box::new(prev_bot),
+                right: Box::new(prev_top),
+                strategy: |left, right, state| {
+                    let l = left.calculate(state)?;
+                    let r = right.calculate(state)?;
+                    Ok(l * r)
+                },
+            });
+        }
+        Operation::Divide => {
+            let prev_top = nodes.pop().unwrap();
+            let prev_bot = nodes.pop().unwrap();
+            nodes.push(Node::Binary {
+                left: Box::new(prev_bot),
+                right: Box::new(prev_top),
+                strategy: |left, right, state| {
+                    let l = left.calculate(state)?;
+                    let r = right.calculate(state)?;
+                    Ok(l / r)
+                },
+            });
         }
     }
 }
@@ -34,8 +111,8 @@ pub fn eval(expression: impl Iterator<Item = char>, state: &State) -> i32 {
     let mut nodes = Vec::new();
     let mut operations = Vec::new();
 
+    let mut expect_operand = true;
     for token in tokens {
-        let mut expect_operand = true;
         match (token, expect_operand) {
             (Token::Operand(OperandsToken::Constant(number)), true) => {
                 nodes.push(Node::Leaf {
@@ -52,79 +129,27 @@ pub fn eval(expression: impl Iterator<Item = char>, state: &State) -> i32 {
                 expect_operand = false;
             }
             (Token::Operation(OperationToken::Plus), true)  => {
-                let prev = nodes.pop().unwrap();
-                nodes.push(
-                    Node::Unary {
-                        child: Box::new(prev),
-                        strategy: |child, state| Ok(child.calculate(state)?),
-                    }
-                );
+                operations.push(Operation::UnaryPlus);
                 expect_operand = true;
             }
             (Token::Operation(OperationToken::Plus), false)  => {
-                let prev_top = nodes.pop().unwrap();
-                let prev_bot = nodes.pop().unwrap();
-                nodes.push(Node::Binary {
-                    left: Box::new(prev_top),
-                    right: Box::new(prev_bot),
-                    strategy: |left, right, state| {
-                        let l = left.calculate(state)?;
-                        let r = right.calculate(state)?;
-                        Ok(l + r)
-                    },
-                });
+                operations.push(Operation::BinaryPlus);
                 expect_operand = true;
             }
             (Token::Operation(OperationToken::Minus), true) => {
-                let prev = nodes.pop().unwrap();
-                nodes.push(
-                    Node::Unary {
-                        child: Box::new(prev),
-                        strategy: |child, state| Ok(-child.calculate(state)?)
-                    }
-                );
+                operations.push(Operation::UnaryMinus);
                 expect_operand = true;
             },
             (Token::Operation(OperationToken::Minus), false) => {
-                let prev_top = nodes.pop().unwrap();
-                let prev_bot = nodes.pop().unwrap();
-                nodes.push(Node::Binary {
-                    left: Box::new(prev_top),
-                    right: Box::new(prev_bot),
-                    strategy: |left, right, state| {
-                        let l = left.calculate(state)?;
-                        let r = right.calculate(state)?;
-                        Ok(l - r)
-                    },
-                });
+                operations.push(Operation::BinaryMinus);
                 expect_operand = true;
             },
             (Token::Operation(OperationToken::Multiply), false) => {
-                let prev_top = nodes.pop().unwrap();
-                let prev_bot = nodes.pop().unwrap();
-                nodes.push(Node::Binary {
-                    left: Box::new(prev_top),
-                    right: Box::new(prev_bot),
-                    strategy: |left, right, state| {
-                        let l = left.calculate(state)?;
-                        let r = right.calculate(state)?;
-                        Ok(l * r)
-                    },
-                });
+                operations.push(Operation::Multiply);
                 expect_operand = true;
             },
             (Token::Operation(OperationToken::Divide), false) => {
-                let prev_top = nodes.pop().unwrap();
-                let prev_bot = nodes.pop().unwrap();
-                nodes.push(Node::Binary {
-                    left: Box::new(prev_top),
-                    right: Box::new(prev_bot),
-                    strategy: |left, right, state| {
-                        let l = left.calculate(state)?;
-                        let r = right.calculate(state)?;
-                        Ok(l / r)
-                    },
-                });
+                operations.push(Operation::Divide);
                 expect_operand = true;
             },
             (Token::OpenParenthesis, true) => {
@@ -138,9 +163,26 @@ pub fn eval(expression: impl Iterator<Item = char>, state: &State) -> i32 {
                 todo!("Unexpected")
             }
         }
+
+        if operations.len() <= 2 {
+            continue;
+        }
+        if let Some(x) = operations.get(operations.len() - 1) {
+            if let Some(y) = operations.get(operations.len() - 2) {
+                if *x > *y {
+                    let operation = operations.pop().unwrap();
+                    collapse(operation, &mut nodes)
+                }
+            }
+        }
+    }
+
+    while let Some(operation) = operations.pop() {
+        collapse(operation, &mut nodes)
     }
 
     println!("{:?}", nodes);
+    println!("{:?}", operations);
 
     42
 }
