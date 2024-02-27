455e0b7dab0a926b2034d12084a078cdbdbd4ee8
Naokitsu <naokitsu@tutanota.com>
2024-02-27 05:06:56 +0400

    Use enum for Node instead of trait

--- src/node.rs
+++ src/node.rs
@@ -1,71 +1,30 @@
-use std::fmt::{Display};
+use std::fmt::{Debug, Display};
 use crate::error::Error;
 use crate::state::State;
 
-trait Node {
-    fn calculate(&self, state: State) -> Result<i32, Error>;
+#[derive(Debug, Clone)]
+enum Node {
+    Leaf {
+        strategy: fn(&State) -> Result<i32, Error>,
+    },
+    Unary {
+        child: Box<Node>,
+        strategy: fn(child: &Box<Node>, &State) -> Result<i32, Error>,
+    },
+    Binary {
+        left: Box<Node>,
+        right: Box<Node>,
+        strategy: fn(left: &Box<Node>, right: &Box<Node>, &State) -> Result<i32, Error>,
+    },
 }
 
-#[derive(Copy, Clone, Debug)]
-struct ConstantNode {
-    value: i32,
-}
-
-
-impl Node for ConstantNode {
-    fn calculate(&self, _: State) -> Result<i32, Error> {
-        Ok(self.value)
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
-struct VariableNode {
-    name: String,
-}
-
-impl Node for VariableNode {
-    fn calculate(&self, state: State) -> Result<i32, Error> {
-        if let Some(x) = state.get(&self.name) {
-            Ok(*x)
-        } else {
-            Err(Error::UninitializedVariable(self.name.clone()))
+impl Node {
+    fn calculate(&self, state: &State) -> Result<i32, Error> {
+        match self {
+            Node::Leaf { strategy } => strategy(state),
+            Node::Unary { child, strategy} => strategy(child, state),
+            Node::Binary { left, right, strategy } => strategy(left, right, state),
         }
     }
 }
 
-#[derive(Copy, Clone, Debug)]
-struct UnaryNode {
-    child: Box<dyn Node>,
-    operator: char,
-}
-
-impl Node for UnaryNode {
-    fn calculate(&self, state: State) -> Result<i32, Error> {
-        let child = self.child.calculate(state)?;
-        match self.operator {
-            '+' => Ok(child),
-            '-' => Ok(-child),
-            _ => Err(Error::InvalidSyntax)
-        }
-    }
-}
-
-struct BinaryNode {
-    left: Box<dyn Node>,
-    right: Box<dyn Node>,
-    operator: char,
-}
-
-impl Node for BinaryNode {
-    fn calculate(&self, state: State) -> Result<i32, Error> {
-        let left = self.left.calculate(state)?;
-        let right = self.right.calculate(state)?;
-        match self.operator {
-            '+' => Ok(left + right),
-            '-' => Ok(left - right),
-            '*' => Ok(left * right),
-            '/' => Ok(left / right),
-            _ => Err(Error::InvalidSyntax)
-        }
-    }
-}
